e2729c3705edc8b04b0db499f2fb3c7d3cdccd60
jiyeong1006 <jiyeong1006@naver.com>
2025-01-13 19:18:07 +0900

    update cd-pipeline

--- test_app.py
+++ test_app.py
@@ -1,3 +1,12 @@
-from app import hello_world
-def test_hello_world():
-    assert hello_world() == "Hello, World!"
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def test_hello(self):
+        tester = app.test_client(self)
+        response = tester.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), "Hello, World!")
+
+if __name__ == "__main__":
+    unittest.main()
